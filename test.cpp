cd26375d9eb40aa66657cd693cc168586bbe6e0c
ia332s11 <iluha154russick@gmail.com>
2025-11-24 23:03:09 +0700

    qpsk

--- test.cpp
+++ test.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <fstream>
 #include <complex>
-#include "head.h"
 
 using namespace std;
 
@@ -102,31 +101,8 @@ vector<int16_t> filter(vector<int16_t> qpsk_symbols, int L, int size){
     return result;
 }
 
-vector<int16_t> m_filter(int16_t tx_buff[2 * 1920], int L, int size){
-    int size_arr = L * size;
-    vector<int16_t> g(L, 1);
-    vector<int16_t> x(size_arr, 0);
-    vector<int16_t> result(size_arr, 0);
-
-    for(int i = 0; i < size_arr; i += L){
-        int idx = i / L;
-        x[i] = tx_buff[idx];
-    }
-    
-    for(int i = 0; i < size_arr; i++){
-        int16_t tmp = 0;
-        for(int m = 0; m < L; m++){
-            if(i - m >= 0){                                 
-                tmp += x[i - m] * g[m];
-            }
-        }
-        result[i] = tmp;
-    }
-    return result;
-}
-
 vector<int16_t> my_ready_samples(){
-    int size = 192;
+    int size = 12;
     int sample_rate = 32;
 
     vector<int16_t> generated_bits = generate_bits(size);
@@ -142,4 +118,47 @@ vector<int16_t> my_ready_samples(){
         }
     }
     return ready_samples;
+}
+
+int main(){
+
+    int16_t tx_mtu = 100;
+    int16_t rx_mtu = 1920;
+
+    int16_t tx_buff[2 *tx_mtu] = {};
+    int16_t rx_buffer[2 *rx_mtu] = {};
+
+    int cur_sample_in_file = 0;
+
+    vector<int16_t> my_samples = my_ready_samples();
+
+    for(int i = 0; i < my_samples.size(); i++){
+        my_samples[i] = my_samples[i] * (2047<<4);
+    }
+
+
+    size_t sample_count = my_samples.size();
+    size_t send_sample = 0;
+
+    FILE *file = fopen("txdata1.pcm", "wb");
+    while(send_sample < sample_count){
+        for(int i = 0; i < tx_mtu * 2; i++){
+            tx_buff[i] = my_samples[send_sample];
+
+
+            printf("%d , ", tx_buff[i]);
+            if(i % 10 == 0){
+                printf("\n");
+            }
+
+            send_sample++;
+            if(send_sample == sample_count){
+                break;
+            }
+        }
+        fwrite(tx_buff, 2* tx_mtu * sizeof(int16_t), 1, file);
+    }
+    printf("samples count = %d \n", send_sample);
+    fclose(file);
+
 }
